1de8bd73b9f6dbb517f30167464879f62c91e902
Unknownt247 <bello.bakeem@gmail.com>
2023-01-24 18:37:38 -0500

    Footer and blog section updated. Work on dividing footer into more react components for future projects

--- src/containers/blog/imports.js
+++ src/containers/blog/imports.js
@@ -1,7 +1,7 @@
-import blog1 from "../../Assets/blog1.jfif"
-import blog2 from "../../Assets/blog2.jfif"
-import blog3 from "../../Assets/blog3.jfif"
-import blog4 from "../../Assets/blog4.jfif"
-import blog5 from "../../Assets/blog5.jfif"
+import blog1 from "../../Assets/blog1.png"
+import blog2 from "../../Assets/blog2.png"
+import blog3 from "../../Assets/blog3.png"
+import blog4 from "../../Assets/blog4.png"
+import blog5 from "../../Assets/blog5.png"
 
 export {blog1, blog2, blog3, blog4, blog5}
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './footer.css'
+import Frame from "../../Assets/Frame.png"
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="gpt3___footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className='gradient_text'>Be among the headliners for the future</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={Frame}/>
+          <p>"Technology is the campfire around which we tell our stories." - Laurie Anderson</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in Touch</h4>
+          <p>bello.bakeem@gmail.com</p>
+          <p>647-687-9039</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>Copyright Bello Inc. All rights Reserved</p>
+      </div>
     </div>
   )
 }
